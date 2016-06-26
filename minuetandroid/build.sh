e1e5237e115d8206d72557578cb1c011ea8ddcab
Ayush Shah <1595ayush@gmail.com>
2016-06-26 23:06:53 +0530

    Fix a minor error that prevented creating share directory

--- minuetandroid/build.sh
+++ minuetandroid/build.sh
@@ -4,7 +4,7 @@ cmake ../ -DCMAKE_TOOLCHAIN_FILE=/usr/share/ECM/toolchain/Android.cmake -DCMAKE_
 
 make
 
-mkdir "${INSTALL_DIR}"/share
+mkdir -p "${INSTALL_DIR}"/share
 mkdir -p "${INSTALL_DIR}"/lib/qml
 
 make install/strip
